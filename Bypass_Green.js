de8c8c40793ca9a161d64f799252a702bd7a9995
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2022-06-04 03:50:11 +0800

    Update Bypass_Green.js

--- Bypass_Green.js
+++ Bypass_Green.js
@@ -9,7 +9,7 @@
 QuantumultX:
 
 [rewrite_local]
-^https:?\/\/whhb\.tgovcloud\.com\/epidemicbg\/vaccine\/info\/v2\?qrcCode\=.  url script-response-body https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/main/Bypass_Green.js
+^https:?\/\/whhb\.tgovcloud\.com\/epidemicbg\/nucleic\/infoV3\?qrcCode\=.+  url script-response-body https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/main/Bypass_Green.js
 
 [mitm]
 hostname = whhw.tgovcloud.com
