9c6d3f10f1106b0f6078e84be036f0745125bcae
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2022-06-04 16:07:20 +0800

    Update Zuotang.js

--- Zuotang.js
+++ Zuotang.js
@@ -18,7 +18,7 @@
 QuantumultX:
 
 [rewrite_local]
-https?:\/\/gw\.aoscdn.com\/base\/vip\/client\/authorizations url response-body .+ url script-response-body user-url
+https?:\/\/gw\.aoscdn.com\/base\/vip\/client\/authorizations url response-body .+ url script-response-body https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/main/Zuotang.js
 
 [mitm]
 hostname = gw.aoscdn.com
