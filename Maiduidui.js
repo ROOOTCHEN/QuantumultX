50b8ed8903177bf2795643d16c21a42fca277797
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2022-06-05 02:41:36 +0800

    Add files via upload

---
+++ Maiduidui.js
@@ -0,0 +1,11 @@
+
+var body = $response.body; //将java对象转为json格式的数据
+var obj = JSON.parse(body); //将JSON格式字符串转换为JavaScript对象
+
+obj.data.isPay = '1';
+obj.data.isVodVip = '1';
+obj.data.vipEffectiveDate = '2099-12-31';
+body = JSON.stringify(obj); //JavaScript 值转换为 JSON 字符串。
+$done({
+	body
+});
