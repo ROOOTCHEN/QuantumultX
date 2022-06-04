8f944882aff2fb1ef623f7c3d7184732c1e8e379
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2022-06-04 23:09:09 +0800

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,13 @@
+var body = $response.body; //
+var url = $request.method;
+var obj = JSON.parse(body);
+
+if (url.method() == '13683707') {
+	obj.data["extra"] =
+		"YzEwZWZjYTE2NmU0ZGZlMjdkNTc5NjcyN2IwOWQwMjAxMTZhMGY5NWZlNzMzNTM3NjZjOWQ2ZWNhMzI2NTNhY2Y1YmQ4NjA0ZTY4OWZjM2Q2MWM2YmJkZmQyYmFlNDY3ZWIyNzQ4NzJhMjM4NDA0OWVhYjMzMmVkNWQ4Nzk0NmFmYTRmMWJiYjhjM2I4M2M5MmIxZjYyODU1NjQ1YmFkOWRjZDU0MTRhZWZmY2FlNGU2Y2ZlODZlZmQ5ODE2MTNkODlmNTFlZmMwMGU2ZGM0NmFhYTYxNzU0M2E2OTkyZDIxNzUxZGEzZjc3YjQ3NDcy";
+	body = JSON.stringify(obj);
+}
+
+$done({
+	body
+});
