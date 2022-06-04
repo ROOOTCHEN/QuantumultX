d6d25cc36ed0c0c7661b5916c25daeaee8b80f60
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2022-06-04 13:52:56 +0800

    Update Bypass_Green.js

--- Bypass_Green.js
+++ Bypass_Green.js
@@ -15,8 +15,6 @@ QuantumultX:
 hostname = whhw.tgovcloud.com
 **************************/
 
-let obj = JSON.parse($response.body);
-obj.data.attributes["color"] = "green";
-$done({
-	body: JSON.stringify(obj)
-});
+var body = $response.body
+    .replace(/\"color\":grey/, "\"color\":green");
+$done({ body });
