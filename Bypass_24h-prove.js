7775cdea50e68b50ca4c34e04c0f4046a2c352e3
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2022-06-04 13:09:08 +0800

    Update Bypass_24h-prove.js

--- Bypass_24h-prove.js
+++ Bypass_24h-prove.js
@@ -1,1 +1,52 @@
+/*
+脚本功能：i武汉 武汉战疫 微信小程序 解锁24小时核酸证明
+脚本作者：RootChen
+更新时间：2022-06-04
 
+***************************
+QuantumultX:
+
+[rewrite_local]
+^https:?\/\/whhb\.tgovcloud\.com\/epidemicbg\/nucleic\/infoV3\?qrcCode\=.+  url script-response-body https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/main/Bypass_24h-prove.js
+
+[mitm]
+hostname = whhw.tgovcloud.com
+**************************/
+
+var dates = new Date();
+var month = dates.getMonth() + 1
+month = month < 10 ? "0" + month : month
+var date = dates.getDate() - 1
+date = date < 10 ? "0" + date : date
+var hours = dates.getHours() - 2
+hours = hours < 10 ? "0" + hours : hours
+var minutes = dates.getMinutes() - 24
+minutes = minutes < 10 ? "0" + minutes : minutes
+var seconds = dates.getSeconds()
+seconds = seconds < 10 ? "0" + seconds : seconds
+var arr = [
+	dates.getFullYear() + '-',
+	month + '-',
+	date + ' ',
+	hours + ':',
+	minutes + ':',
+	seconds
+]
+var arr = arr.join('')
+console.log(arr);
+
+if ($response.body) {
+	$done({
+		body: JSON.stringify({
+			"msg": "执行成功",
+			"code": 0,
+			"data": [{
+				"detectResult": "阴性",
+				"detectTime": arr,
+				"source": "2"
+			}]
+		})
+	});
+} else {
+	$done({});
+}
