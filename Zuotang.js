a38544660ea6bdf5004ea2ad2479830957f8107c
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2022-06-04 16:01:00 +0800

    Update Zuotang.js

--- Zuotang.js
+++ Zuotang.js
@@ -1,15 +1,55 @@
 /*
-佐糖1.1.6解锁VIP
-App Store：https://apps.apple.com/cn/app/picwish/id1609584814
-解锁会员全部功能
+脚本功能：解锁 佐糖&佐糖照片修复&傲软抠图 VIP功能
+
+佐糖 1.1.6
+下载地址：https://apps.apple.com/cn/app/id1609584814
+
+佐糖照片修复 2.0.5
+下载地址：https://apps.apple.com/cn/app/id1590983966
+
+傲软抠图 1.5.9
+下载地址：https://apps.apple.com/cn/app/id1490054676
+
+脚本作者：RootChen
+更新时间：2022-06-04
+使用声明：️此脚本仅供学习与交流
+
 ***************************
 QuantumultX:
 
 [rewrite_local]
-^https?:\/\/gw\.aoscdn.com\/base\/vip\/client\/authorizations url script-response-body https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/main/Zuotang.js
+https?:\/\/gw\.aoscdn.com\/base\/vip\/client\/authorizations url response-body .+ url script-response-body user-url
 
 [mitm]
 hostname = gw.aoscdn.com
 **************************/
 
-var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Oxe2244=["\x62\x6F\x64\x79","\x73\x75\x63\x63\x65\x73\x73","\x70\x65\x72\x73\x6F\x6E\x61\x6C","\x64\x61\x69\x6C\x79","\x32\x30\x39\x39\x2D\x31\x32\x2D\x33\x31\x20\x31\x39\x3A\x33\x35\x3A\x35\x30","\x73\x74\x72\x69\x6E\x67\x69\x66\x79","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];if($response[__Oxe2244[0x0]]){$done({body:JSON[__Oxe2244[0x5]]({"\x73\x74\x61\x74\x75\x73":200,"\x6D\x65\x73\x73\x61\x67\x65":__Oxe2244[0x1],"\x64\x61\x74\x61":{"\x6C\x69\x63\x65\x6E\x73\x65\x5F\x74\x79\x70\x65":__Oxe2244[0x2],"\x70\x65\x72\x69\x6F\x64\x5F\x74\x79\x70\x65":__Oxe2244[0x3],"\x69\x73\x5F\x61\x63\x74\x69\x76\x61\x74\x65\x64":1,"\x72\x65\x6D\x61\x69\x6E\x5F\x64\x61\x79\x73":0,"\x77\x69\x6C\x6C\x5F\x65\x78\x70\x69\x72\x65":1,"\x61\x6C\x6C\x6F\x77\x65\x64\x5F\x64\x65\x76\x69\x63\x65\x5F\x63\x6F\x75\x6E\x74":1,"\x62\x65\x67\x69\x6E\x5F\x61\x63\x74\x69\x76\x61\x74\x65\x64\x5F\x74\x69\x6D\x65":1654196185,"\x64\x75\x72\x61\x74\x69\x6F\x6E\x73":0,"\x76\x69\x70\x5F\x73\x70\x65\x63\x69\x61\x6C":0,"\x68\x61\x73\x5F\x62\x75\x79\x5F\x65\x78\x74\x65\x6E\x64":0,"\x68\x61\x73\x5F\x70\x72\x65\x73\x65\x6E\x74":0,"\x70\x72\x6F\x64\x75\x63\x74\x5F\x69\x64":480,"\x69\x73\x5F\x6C\x69\x66\x65\x74\x69\x6D\x65":0,"\x65\x78\x70\x69\x72\x65\x64\x5F\x61\x74":1685993750,"\x65\x78\x70\x69\x72\x65\x5F\x74\x69\x6D\x65":__Oxe2244[0x4],"\x64\x65\x76\x69\x63\x65\x5F\x69\x64":300402468,"\x65\x78\x69\x73\x74\x5F\x74\x72\x69\x61\x6C":1}})})}else {$done({})};;;(function(_0xa470x1,_0xa470x2,_0xa470x3,_0xa470x4,_0xa470x5,_0xa470x6){_0xa470x6= __Oxe2244[0x6];_0xa470x4= function(_0xa470x7){if( typeof alert!== _0xa470x6){alert(_0xa470x7)};if( typeof console!== _0xa470x6){console[__Oxe2244[0x7]](_0xa470x7)}};_0xa470x3= function(_0xa470x8,_0xa470x1){return _0xa470x8+ _0xa470x1};_0xa470x5= _0xa470x3(__Oxe2244[0x8],_0xa470x3(_0xa470x3(__Oxe2244[0x9],__Oxe2244[0xa]),__Oxe2244[0xb]));try{_0xa470x1= __encode;if(!( typeof _0xa470x1!== _0xa470x6&& _0xa470x1=== _0xa470x3(__Oxe2244[0xc],__Oxe2244[0xd]))){_0xa470x4(_0xa470x5)}}catch(e){_0xa470x4(_0xa470x5)}})({})
+if ($response.body) {
+	$done({
+		body: JSON.stringify({
+			"status": 200,
+			"message": "success",
+			"data": {
+				"license_type": "personal",
+				"period_type": "daily",
+				"is_activated": 1,
+				"remain_days": 0,
+				"will_expire": 1,
+				"allowed_device_count": 1,
+				"begin_activated_time": 1654196185,
+				"durations": 0,
+				"vip_special": 0,
+				"has_buy_extend": 0,
+				"has_present": 0,
+				"product_id": 480,
+				"is_lifetime": 0,
+				"expired_at": 1685993750,
+				"expire_time": "2099-12-31 19:35:50",
+				"device_id": 300402468,
+				"exist_trial": 1
+			}
+		})
+	});
+} else {
+	$done({});
+}
