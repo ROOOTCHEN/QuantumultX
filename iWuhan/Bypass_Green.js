52f6bf88699a71180aac9030580c4b11669bca45
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2022-07-27 03:24:01 +0800

    Update Bypass_Green.js

--- iWuhan/Bypass_Green.js
+++ iWuhan/Bypass_Green.js
@@ -1,6 +1,5 @@
 /*
-
-脚本功能：i武汉 武汉战疫 微信小程序 灰码&红码转绿码
+脚本功能：i武汉_解锁灰码专转绿码
 脚本作者：RootChen
 更新时间：2022-06-04
 使用声明：️此脚本仅供学习与交流
@@ -11,8 +10,6 @@ QuantumultX:
 [rewrite_local]
 ^https:?\/\/whhb\.tgovcloud\.com\/epidemicbg\/rylxdmInfo\/v2\/get\?codeId=.+ url script-response-body https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/main/iWuhan/Bypass_Green.js
 
-[mitm]
-hostname = whhw.tgovcloud.com
 
 var body = $response.body; //将java对象转为json格式的数据
 var obj = JSON.parse(body); //将JSON格式字符串转换为JavaScript对象
