298e84338920248d8a817e1dd1251901f939050d
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2022-07-27 03:24:10 +0800

    Update Bypass_Vaccines.js

--- iWuhan/Bypass_Vaccines.js
+++ iWuhan/Bypass_Vaccines.js
@@ -1,19 +1,14 @@
 /*
-
-脚本功能：i武汉 武汉战疫 微信小程序 伪装
+脚本功能：i武汉_解锁接种疫苗三针
 脚本作者：RootChen
 更新时间：2022-06-04
 使用声明：️此脚本仅供学习与交流
 
 ***************************
 QuantumultX:
-**************************/
+***************************/
 [rewrite_local]
 ^https:?\/\/whhb\.tgovcloud\.com\/epidemicbg\/vaccine\/info\/v2\?qrcCode\=.+ url script-response-body https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/main/iWuhan/Bypass_Vaccines.js
-
-[mitm]
-hostname = whhw.tgovcloud.com
-
 
 
 if ($response.body) {
