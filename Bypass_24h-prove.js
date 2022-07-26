146a607a376038a201618ad701d79b543edf65e1
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2022-07-27 01:32:39 +0800

    Update Bypass_24h-prove.js

--- Bypass_24h-prove.js
+++ Bypass_24h-prove.js
@@ -3,6 +3,7 @@
 脚本功能：i武汉 武汉战疫 微信小程序 解锁24小时核酸证明
 脚本作者：RootChen
 更新时间：2022-06-04
+更新时间：2022-07-27
 
 ***************************
 QuantumultX:
@@ -17,13 +18,13 @@ hostname = whhw.tgovcloud.com
 var dates = new Date();
 var month = dates.getMonth() + 1
 month = month < 10 ? "0" + month : month
-var date = dates.getDate() -1
+var date = dates.getDate() - 1
 date = date < 10 ? "0" + date : date
-var hours = dates.getHours() +2
+var hours = dates.getHours() + 2
 hours = hours < 10 ? "0" + hours : hours
 var minutes = dates.getMinutes()
 minutes = minutes < 10 ? "0" + minutes : minutes
-var seconds = dates.getSeconds() 
+var seconds = dates.getSeconds()
 seconds = seconds < 10 ? "0" + seconds : seconds
 var arr = [
 	dates.getFullYear() + '-',
@@ -42,9 +43,11 @@ if ($response.body) {
 			"msg": "执行成功",
 			"code": 0,
 			"data": [{
-				"detectResult": "阴性",
 				"detectTime": arr,
-				"source": "2"
+				"source": "2",
+				"detectOrg": "武汉国药医学检验实验室",
+				"detectResult": "阴性"
+
 			}]
 		})
 	});
