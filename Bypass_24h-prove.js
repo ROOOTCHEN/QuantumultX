fac3fed7ca26145e93486405124d7a3c188db760
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2022-07-27 02:30:17 +0800

    Update Bypass_24h-prove.js

--- Bypass_24h-prove.js
+++ Bypass_24h-prove.js
@@ -7,13 +7,12 @@
 
 ***************************
 QuantumultX:
-
+**************************/
 [rewrite_local]
 ^https:?\/\/whhb\.tgovcloud\.com\/epidemicbg\/nucleic\/infoV3\?qrcCode\=.+  url script-response-body https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/main/Bypass_24h-prove.js
 
 [mitm]
 hostname = whhw.tgovcloud.com
-**************************/
 
 var dates = new Date();
 var month = dates.getMonth() + 1
