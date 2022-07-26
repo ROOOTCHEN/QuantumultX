83f6fbfe3ab788784ff1b01d76369807e9334701
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2022-07-27 02:31:05 +0800

    Update Bypass_Green.js

--- Bypass_Green.js
+++ Bypass_Green.js
@@ -7,13 +7,13 @@
 
 ***************************
 QuantumultX:
-
+**************************/
 [rewrite_local]
 ^https:?\/\/whhb\.tgovcloud\.com\/epidemicbg\/rylxdmInfo\/v2\/get\?codeId=.+ url script-response-body https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/main/Bypass_Green.js
 
 [mitm]
 hostname = whhw.tgovcloud.com
-**************************/
+
 var body = $response.body; //将java对象转为json格式的数据
 var obj = JSON.parse(body); //将JSON格式字符串转换为JavaScript对象
 
