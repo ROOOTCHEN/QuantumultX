9b987ebbff6ad6c3533ba4d9d29d129554d798f2
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2022-12-05 16:24:24 +0800

    Update bypass_give.js

--- soulapp/bypass_give.js
+++ soulapp/bypass_give.js
@@ -11,7 +11,7 @@ hostname = *.soulapp.cn
 
 var body = $response.body; //将java对象转为json格式的数据
 var obj = JSON.parse(body); //将JSON格式字符串转换为JavaScript对象
-obj.data.limit = 'false';	//强制
+obj.data.limit = false;	//强制
 
 body = JSON.stringify(obj); //JavaScript 值转换为 JSON 字符串。
 $done({
