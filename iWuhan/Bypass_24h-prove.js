d8ced450912a2d6326d9da3fd4efd2ac92106c54
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2023-05-26 23:30:49 +0800

    Update Bypass_24h-prove.js

--- iWuhan/Bypass_24h-prove.js
+++ iWuhan/Bypass_24h-prove.js
@@ -8,8 +8,7 @@
 QuantumultX:
 
 [rewrite_local]
-^https:?\/\/whhb\.tgovcloud\.com\/epidemicbg\/nucleic\/infoV(2|3)\?qrcCode\=.+ url script-response-body https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/main/iWuhan/Bypass_24h-prove.js
-
+^https:?\/\/whhb\.tgovcloud\.com\/epidemicbg\/nucleic\/infoV2\?qrcCode\=.+ url script-response-body https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/main/iWuhan/Bypass_24h-prove.js
 [mitm] 
 hostname = whhb.tgovcloud.com
 
@@ -17,40 +16,30 @@ hostname = whhb.tgovcloud.com
 
 
 var dates = new Date();
-var month = dates.getMonth() + 1
-month = month < 10 ? "0" + month : month
-var date = dates.getDate() - 1
-date = date < 10 ? "0" + date : date
-var hours = dates.getHours() + 2
-hours = hours < 10 ? "0" + hours : hours
-var minutes = dates.getMinutes()
-minutes = minutes < 10 ? "0" + minutes : minutes
-var seconds = dates.getSeconds()
-seconds = seconds < 10 ? "0" + seconds : seconds
-var arr = [
-	dates.getFullYear() + '-',
-	month + '-',
-	date + ' ',
-	hours + ':',
-	minutes + ':',
-	seconds
-]
-var arr = arr.join('')
+dates.setHours(dates.getHours() - 2);
+
+var month = String(dates.getMonth() + 1).padStart(2, '0');
+var date = String(dates.getDate()).padStart(2, '0');
+var hours = String(dates.getHours()).padStart(2, '0');
+var minutes = String(dates.getMinutes()).padStart(2, '0');
+var seconds = String(dates.getSeconds()).padStart(2, '0');
+
+var arr = `${dates.getFullYear()}-${month}-${date} ${hours}:${minutes}:${seconds}`;
 console.log(arr);
 
 if ($response.body) {
-	$done({
-		body: JSON.stringify({
-			"msg": "执行成功",
-			"code": 0,
-			"data": [{
-				"detectOrg": "武汉希望组医学检验实验室",
-				"detectResult": "阴性",
-				"detectTime": arr,
-				"source": "2"
-			}]
-		})
-	});
+  $done({
+    body: JSON.stringify({
+      "msg": "执行成功",
+      "code": 0,
+      "data": [{
+        "detectOrg": "武汉希望组医学检验实验室",
+        "detectResult": "阴性",
+        "detectTime": arr,
+        "source": "2"
+      }]
+    })
+  });
 } else {
-	$done({});
+  $done({});
 }
