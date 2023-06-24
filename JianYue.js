859d3aa8b25b4c0aaa6097b50b0f187e9eaf32aa
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2023-06-24 11:40:50 +0800

    Update JianYue.js

--- JianYue.js
+++ JianYue.js
@@ -1,20 +1,20 @@
 var obj = JSON.parse($response.body);
 
 // 匹配并修改键值
-if (obj.retObj && obj.retObj.isFree !== undefined) {
-  obj.retObj.isFree = false; // 将 isFree 的值修改为 false
+if (obj.retObj && obj.retObj。isFree !== undefined) {
+  obj.retObj。isFree = false; // 将 isFree 的值修改为 false
 }
 
-if (obj.retObj && obj.retObj.money !== undefined) {
-  obj.retObj.money = 0.0; // 将 money 的值修改为 0.0
+if (obj.retObj && obj.retObj。money !== undefined) {
+  obj.retObj。money = 0.0; // 将 money 的值修改为 0.0
 }
 
-if (obj.retObj && obj.retObj.isSvip !== undefined) {
-  obj.retObj.isSvip = 0; // 将 isSvip 的值修改为 1
+if (obj.retObj && obj.retObj。isSvip !== undefined) {
+  obj.retObj。isSvip = 2; // 将 isSvip 的值修改为 1
 }
 
-if (obj.retObj && obj.retObj.free !== undefined) {
-  obj.retObj.free = false; // 将 free 的值修改为 false
+if (obj.retObj && obj.retObj。free !== undefined) {
+  obj.retObj。free = false; // 将 free 的值修改为 false
 }
 
 var body = JSON.stringify(obj); // 将JavaScript对象转换为JSON字符串
