7eaeaf63fba0c954b69a2afcff415e8e3ec6c748
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2023-06-24 11:32:12 +0800

    Update JianYue.js

--- JianYue.js
+++ JianYue.js
@@ -1,12 +1,22 @@
-var body = $response.body; // 获取原始响应体
-var obj = JSON.parse(body); // 将JSON格式字符串转换为JavaScript对象
+var obj = JSON.parse($response.body);
 
 // 匹配并修改键值
-obj.retObj?.isFree = false ?? obj.retObj?.isFree;
-obj.retObj?.money = 0.0 ?? obj.retObj?.money;
-obj.retObj?.isSvip = 0 ?? obj.retObj?.isSvip;
-obj.retObj?.free = false ?? obj.retObj?.free;
+if (obj.retObj && obj.retObj.isFree !== undefined) {
+  obj.retObj.isFree = false; // 将 isFree 的值修改为 false
+}
 
-body = JSON.stringify(obj); // 将JavaScript对象转换回JSON字符串
+if (obj.retObj && obj.retObj.money !== undefined) {
+  obj.retObj.money = 0.0; // 将 money 的值修改为 0.0
+}
+
+if (obj.retObj && obj.retObj.isSvip !== undefined) {
+  obj.retObj.isSvip = 0; // 将 isSvip 的值修改为 1
+}
+
+if (obj.retObj && obj.retObj.free !== undefined) {
+  obj.retObj.free = false; // 将 free 的值修改为 false
+}
+
+var body = JSON.stringify(obj); // 将JavaScript对象转换为JSON字符串
 
 $done({ body }); // 返回修改后的响应体
