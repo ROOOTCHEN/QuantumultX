55602bc9fdd0184664352b34245ab91c6d7dbba2
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2023-06-30 00:42:44 +0800

    Update Bypass_Green.js

--- iWuhan/Bypass_Green.js
+++ iWuhan/Bypass_Green.js
@@ -1,5 +1,5 @@
 /*
-脚本功能：i武汉_解锁灰码专转绿码
+脚本功能：i武汉_解锁灰码转绿码
 脚本作者：RootChen
 更新时间：2022-06-04
 使用声明：️此脚本仅供学习与交流
@@ -18,7 +18,7 @@ hostname = whhb.tgovcloud.com
 var body = $response.body; //将java对象转为json格式的数据
 var obj = JSON.parse(body); //将JSON格式字符串转换为JavaScript对象
 
-obj.data.color = 'green';
+obj.data。color = 'green';
 
 body = JSON.stringify(obj); //JavaScript 值转换为 JSON 字符串。
 $done({
