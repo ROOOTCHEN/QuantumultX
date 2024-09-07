62cfed4f261d07120649fc7b16f33fb40ec1d2ce
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2024-09-07 10:38:55 +0800

    Update lt.js

--- lt.js
+++ lt.js
@@ -25,52 +25,96 @@ hostname = m.client.10010.com
     "otherProductInfo": [
       {
         "orderTime": "",
-        "productId": "91233843",
+        "productId": "91233837",
         "endDate": "",
         "productFee": "",
         "cancelFlag": "4",
-        "productName": "国内流量任性用",
+        "productName": "全国流量任性用",
+        "startDate": "2024-09-07 00:00:00"
+      },
+      {
+        "orderTime": "",
+        "productId": "91233838",
+        "endDate": "",
+        "productFee": "",
+        "cancelFlag": "4",
+        "productName": "短信网龄升级包",
+        "startDate": "2024-09-07 00:00:00"
+      },
+      {
+        "orderTime": "",
+        "productId": "91233839",
+        "endDate": "",
+        "productFee": "",
+        "cancelFlag": "4",
+        "productName": "活动赠送国内语音",
+        "startDate": "2024-09-07 00:00:00"
+      },
+      {
+        "orderTime": "",
+        "productId": "91233840",
+        "endDate": "",
+        "productFee": "",
+        "cancelFlag": "4",
+        "productName": "0元5G极速服务包",
+        "startDate": "2024-09-07 00:00:00"
+      },
+      {
+        "orderTime": "",
+        "productId": "91233841",
+        "endDate": "",
+        "productFee": "",
+        "cancelFlag": "4",
+        "productName": "套内资费减免包",
+        "startDate": "2024-09-07 00:00:00"
+      },
+      {
+        "orderTime": "",
+        "productId": "91233842",
+        "endDate": "",
+        "productFee": "",
+        "cancelFlag": "4",
+        "productName": "联通PLUS黄金会员月卡0元/月（次月生效）",
         "startDate": "2024-09-07 00:00:00"
       }
-      
     ],
     "failureSheetInfo": [],
     "contractInfo": [],
     "netType": "11",
     "NewOrderedTip": "1、0元产品或非自动续订类产品，到期自动失效，无需退订；</br>2、部分合约产品，因涉及违约金的计算和收取，系统功能正逐步完善中，现阶段您可以到归属地线下营业厅进行办理，给您带来的不便，敬请谅解；</br>3、主套餐及套餐内包含的产品不可退订；</br>4、套餐详情可以前往[我的套餐]进行查看；</br>5、个别无法退订的产品，可能由于归属省分业务限制，具体详询10010。</br>6、功能产品列均为免费服务，部分收费产品需基于该列服务能力使用。",
-    "commdityName": "腾讯大王卡",
+    "commdityName": "联通造梦卡",
     "productStartDate": "2018-04-26",
     "liuLiangProductInfo": [
       {
         "ziClose": "0",
         "zifeiflag": "0",
-        "showZf": "0",
+        "showZf": "1",
         "productId": "90063345",
         "endDate": "2029-12-31 23:59:59",
         "iscanCel": "",
         "discntInfo": [
           {
-            "ziClose": "1",
+            "ziClose": "0",
             "discntCode": "5990170",
             "discntForce": "0",
             "endDate": "2029-12-31 23:59:59",
             "discntName": "来电显示服务100%优惠",
             "maindiScnt": "0",
-            "startDate": "2018-04-26 18:46:25"
+            "startDate": "2024-09-07 18:46:25"
           }
         ],
         "productMode": "00",
         "packageId": "59999768",
         "cancelFlag": "0",
-        "productName": "腾讯大王卡",
+        "productName": "联通造梦卡",
         "isPackageUnsubscribe": "3",
         "packageForce": "0",
         "packageName": "来电显示包（赠送）",
-        "startDate": "2018-04-26 18:46:25"
+        "startDate": "2024-09-07 18:46:25"
       },
       {
         "zifeiflag": "0",
-        "showZf": "0",
+        "showZf": "1",
         "productId": "90063345",
         "endDate": "2029-12-31 23:59:59",
         "iscanCel": "",
@@ -88,7 +132,7 @@ hostname = m.client.10010.com
         "productMode": "00",
         "packageId": "51605442",
         "cancelFlag": "0",
-        "productName": "腾讯大王卡",
+        "productName": "联通造梦卡",
         "isPackageUnsubscribe": "3",
         "packageForce": "0",
         "packageName": "岁末年初500M流量包活动（全国）",
@@ -102,34 +146,6 @@ hostname = m.client.10010.com
     "queryTime": "2024/08/29 15:41:40",
     "serviceinfo": [
       {
-        "productmode": "00",
-        "completedate": "20240816093344",
-        "servicestate": "1",
-        "ordermethod": "",
-        "productid": "90063345",
-        "discntvalue": "",
-        "packageid": "59999793",
-        "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
-        "servicename": "呼叫保持",
-        "serviceid": "50007",
-        "completedateFmt": "2024-08-16 09:33:44"
-      },
-      {
-        "productmode": "00",
-        "completedate": "20240816093344",
-        "servicestate": "1",
-        "ordermethod": "",
-        "productid": "90063345",
-        "discntvalue": "",
-        "packageid": "59999793",
-        "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
-        "servicename": "呼叫等待",
-        "serviceid": "50006",
-        "completedateFmt": "2024-08-16 09:33:44"
-      },
-      {
         "productmode": "01",
         "completedate": "20240612194355",
         "servicestate": "1",
@@ -139,23 +155,9 @@ hostname = m.client.10010.com
         "packageid": "53066731",
         "packagename": "5G优享服务包（湖北）",
         "productname": "5G优享服务包（湖北）",
-        "servicename": "5G上网服务(下行峰值500Mbps)",
+        "servicename": "5G上网服务(下行峰值2Gbps)",
         "serviceid": "53546",
         "completedateFmt": "2024-06-12 19:43:55"
-      },
-      {
-        "productmode": "01",
-        "completedate": "20240301000000",
-        "servicestate": "1",
-        "ordermethod": "",
-        "productid": "91259079",
-        "discntvalue": "",
-        "packageid": "52971694",
-        "packagename": "5GB轻合约流量礼包（12个月）",
-        "productname": "5GB轻合约流量礼包（12个月）",
-        "servicename": "5G上网服务（下行峰值300Mbps）",
-        "serviceid": "53545",
-        "completedateFmt": "2024-03-01 00:00:00"
       },
       {
         "productmode": "",
@@ -167,7 +169,7 @@ hostname = m.client.10010.com
         "packageid": "52463303",
         "packagename": "5G网络服务质量VIP叠加包",
         "productname": "",
-        "servicename": "5G网络服务质量VIP",
+        "servicename": "5G网络服务质量VVIP",
         "serviceid": "50867",
         "completedateFmt": "2023-11-01 00:00:00"
       },
@@ -180,7 +182,7 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999793",
         "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "5G服务（SA）",
         "serviceid": "50334",
         "completedateFmt": "2021-09-24 22:47:55"
@@ -194,7 +196,7 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999793",
         "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "流量封顶取消",
         "serviceid": "50303",
         "completedateFmt": "2020-10-10 03:34:02"
@@ -208,7 +210,7 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999793",
         "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "VoLTE",
         "serviceid": "50106",
         "completedateFmt": "2019-12-11 17:53:43"
@@ -222,10 +224,10 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999793",
         "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "5G/4G/3G流量提醒",
         "serviceid": "50300",
-        "completedateFmt": "2018-04-26 18:46:25"
+        "completedateFmt": "2024-09-07 18:46:25"
       },
       {
         "productmode": "00",
@@ -236,10 +238,10 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999793",
         "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "4G+产品服务",
         "serviceid": "50107",
-        "completedateFmt": "2018-04-26 18:46:25"
+        "completedateFmt": "2024-09-07 18:46:25"
       },
       {
         "productmode": "00",
@@ -250,10 +252,10 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999793",
         "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "手机上网服务",
         "serviceid": "50100",
-        "completedateFmt": "2018-04-26 18:46:25"
+        "completedateFmt": "2024-09-07 18:46:25"
       },
       {
         "productmode": "00",
@@ -264,10 +266,38 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999793",
         "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "不可及前转",
         "serviceid": "50022",
-        "completedateFmt": "2018-04-26 18:46:25"
+        "completedateFmt": "2024-09-07 18:46:25"
+      },
+      {
+        "productmode": "00",
+        "completedate": "20240816093344",
+        "servicestate": "1",
+        "ordermethod": "",
+        "productid": "90063345",
+        "discntvalue": "",
+        "packageid": "59999793",
+        "packagename": "基本业务功能包",
+        "productname": "联通造梦卡",
+        "servicename": "呼叫保持",
+        "serviceid": "50007",
+        "completedateFmt": "2024-09-07 18:46:25"
+      },
+      {
+        "productmode": "00",
+        "completedate": "20240816093344",
+        "servicestate": "1",
+        "ordermethod": "",
+        "productid": "90063345",
+        "discntvalue": "",
+        "packageid": "59999793",
+        "packagename": "基本业务功能包",
+        "productname": "联通造梦卡",
+        "servicename": "呼叫等待",
+        "serviceid": "50006",
+        "completedateFmt": "2024-09-07 18:46:25"
       },
       {
         "productmode": "00",
@@ -278,10 +308,10 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999793",
         "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "无应答前转",
         "serviceid": "50021",
-        "completedateFmt": "2018-04-26 18:46:25"
+        "completedateFmt": "2024-09-07 18:46:25"
       },
       {
         "productmode": "00",
@@ -292,10 +322,10 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999793",
         "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "遇忙前转",
         "serviceid": "50020",
-        "completedateFmt": "2018-04-26 18:46:25"
+        "completedateFmt": "2024-09-07 18:46:25"
       },
       {
         "productmode": "00",
@@ -306,10 +336,10 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999793",
         "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "国内漫游",
         "serviceid": "50014",
-        "completedateFmt": "2018-04-26 18:46:25"
+        "completedateFmt": "2024-09-07 18:46:25"
       },
       {
         "productmode": "00",
@@ -320,10 +350,10 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999793",
         "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "国内长途",
         "serviceid": "50010",
-        "completedateFmt": "2018-04-26 18:46:25"
+        "completedateFmt": "2024-09-07 18:46:25"
       },
       {
         "productmode": "00",
@@ -334,10 +364,10 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999768",
         "packagename": "来电显示包（赠送）",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "来电显示",
         "serviceid": "50004",
-        "completedateFmt": "2018-04-26 18:46:25"
+        "completedateFmt": "2024-09-07 18:46:25"
       },
       {
         "productmode": "00",
@@ -348,10 +378,10 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999793",
         "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "短信收发",
         "serviceid": "50003",
-        "completedateFmt": "2018-04-26 18:46:25"
+        "completedateFmt": "2024-09-07 18:46:25"
       },
       {
         "productmode": "00",
@@ -362,10 +392,10 @@ hostname = m.client.10010.com
         "discntvalue": "",
         "packageid": "59999793",
         "packagename": "基本业务功能包",
-        "productname": "腾讯大王卡",
+        "productname": "联通造梦卡",
         "servicename": "语音主服务",
         "serviceid": "50000",
-        "completedateFmt": "2018-04-26 18:46:25"
+        "completedateFmt": "2024-09-07 18:46:25"
       }
     ],
     "csImg": "https://m1.img.10010.com/resources/7188192A31B5AE06E41B64DA6D65A9B0/20200422/png/20200422151005.png",
