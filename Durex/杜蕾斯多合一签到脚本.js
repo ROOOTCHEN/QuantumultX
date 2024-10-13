85533723420526894f3d21330268f69508140320
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2024-10-13 21:52:17 +0800

    Update 杜蕾斯多合一签到脚本.js

--- Durex/杜蕾斯多合一签到脚本.js
+++ Durex/杜蕾斯多合一签到脚本.js
@@ -7,7 +7,6 @@
 #!homepage =  https://github.com/ROOOTCHEN/QuantumultX
 #!loon_version = 3.2.3(762)
 
-
 [Argument]
 arg0 = switch,true,tag=杜蕾斯Cookie-开关
 
@@ -20,7 +19,6 @@ arg4 = switch,true,tag=幸运转转转10月送好礼-开关
 arg5 = input,"0 0 * * *",tag=杜蕾斯每日签到-Cron
 arg6 = switch,true,tag=杜蕾斯每日签到-开关
 
-
 [Script]
 #会员宠粉遇好礼
 cron {arg1} script-path=https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/refs/heads/main/Durex/Durex_Task01.js, enable= {arg2}, tag=会员宠粉遇好礼
@@ -32,7 +30,7 @@ cron {arg3} script-path=https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/
 cron {arg5} script-path=https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/dls.js, enable= {arg6}, tag=杜蕾斯每日签到
 
 #获取Cookie
-http-request ^https:\/\/vip\.ixiliu\.cn\/mp\/user\/info$, script-path=https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/dls.js, enable= {arg0},requires-body=true, timeout=60, tag=杜蕾斯Cookie
+http-request ^https:\/\/vip\.ixiliu\.cn\/mp\/user\/info$ script-path=https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/dls.js,enable= {arg0}, timeout=60, tag=杜蕾斯Cookie
 
 [MITM]
 hostname = vip.ixiliu.cn
