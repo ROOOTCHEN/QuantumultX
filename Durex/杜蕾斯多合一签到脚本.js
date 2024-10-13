ee2a1e1d44c6abec7ad922de288eec7471f09b76
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2024-10-13 21:30:58 +0800

    Update 杜蕾斯多合一签到脚本.js

--- Durex/杜蕾斯多合一签到脚本.js
+++ Durex/杜蕾斯多合一签到脚本.js
@@ -1,19 +1,26 @@
 #!name = 杜蕾斯签到合集
 #!desc = 知名国际运动品牌签到-定时任务
+#!desc = 由Root整合修改,版权归原作者
 #!data = 2024-10-13
-#!icon=https://raw.githubusercontent.com/czy13724/LeviIcons/main/leviicons/dls.png
+#!tag = 签到
+#!icon = https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/refs/heads/main/Durex/Durex-Logo.jpg
+#!author = RootChen[https://t.me/RoootChen],Levi[https://levifree.tech/]
+#!homepage =  https://github.com/ROOOTCHEN/QuantumultX
+#!loon_version = 3.2.3(762)
+
 
 [Argument]
-arg1 = input,"0 0 1-3 * *",tag=会员宠粉遇好礼-开关
+arg0 = switch,true,tag=杜蕾斯Cookie-开关
+
+arg1 = input,"0 0 1-3 * *",tag=会员宠粉遇好礼-Cron
 arg2 = switch,true,tag=会员宠粉遇好礼-开关
 
-arg3 = input,"0 0 * * *",tag=幸运转转转10月送好礼
+arg3 = input,"0 0 * * *",tag=幸运转转转10月送好礼-Cron
 arg4 = switch,true,tag=幸运转转转10月送好礼-开关
 
-arg5 = input,"0 0 * * *",tag=杜蕾斯每日签到
+arg5 = input,"0 0 * * *",tag=杜蕾斯每日签到-Cron
 arg6 = switch,true,tag=杜蕾斯每日签到-开关
 
-arg7 = switch,true,tag=杜蕾斯COOKIE-开关
 
 [Script]
 cron {arg1} script-path=https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/refs/heads/main/Durex/Durex_Task01.js, enable= {arg2}, tag=会员宠粉遇好礼
@@ -22,7 +29,7 @@ cron {arg3} script-path=https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/
 
 cron {arg5} script-path=https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/dls.js, enable= {arg6}, tag=杜蕾斯每日签到
 
-http-request ^https:\/\/vip\.ixiliu\.cn\/mp\/sign\/applyV2$,script-path=https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/dls.js, enable= {arg7},requires-body=true, timeout=60, tag=杜蕾斯Cookie
+http-request ^https:\/\/vip\.ixiliu\.cn\/mp\/sign\/applyV2$,script-path=https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/dls.js, enable= {arg0},requires-body=true, timeout=60, tag=杜蕾斯Cookie
 
 [MITM]
 hostname = vip.ixiliu.cn
