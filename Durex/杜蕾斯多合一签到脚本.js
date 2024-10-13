6a57a2ca442603f8f751536f6eea4d2b0dad935a
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2024-10-13 22:49:16 +0800

    Update 杜蕾斯多合一签到脚本.js

--- Durex/杜蕾斯多合一签到脚本.js
+++ Durex/杜蕾斯多合一签到脚本.js
@@ -8,29 +8,31 @@
 #!loon_version = 3.2.3(762)
 
 [Argument]
-arg0 = switch,true,tag=杜蕾斯Cookie-开关
+cookie = switch,true,tag=杜蕾斯获取Token
 
-arg1 = input,"0 0 1-3 * *",tag=会员宠粉遇好礼-Cron
-arg2 = switch,true,tag=会员宠粉遇好礼-开关
+cron1 = input,"0 0 1-3 * *",tag=会员宠粉遇好礼 定时任务,desc=请输入定时任务的cron表达式
+durex1= switch,true,tag=会员宠粉遇好礼 任务开关
 
-arg3 = input,"0 0 * * *",tag=幸运转转转10月送好礼-Cron
-arg4 = switch,true,tag=幸运转转转10月送好礼-开关
 
-arg5 = input,"0 0 * * *",tag=杜蕾斯每日签到-Cron
-arg6 = switch,true,tag=杜蕾斯每日签到-开关
+cron2 = input,"0 0 * * *",tag=幸运转转转10月送好礼 定时任务,desc=请输入定时任务的cron表达式
+durex2 = switch,true,tag=幸运转转转10月送好礼 任务开关
+
+
+cron3 = input,"0 0 * * *",tag=杜蕾斯每日签到 定时任务,desc=请输入定时任务的cron表达式
+durex3 = switch,true,tag=杜蕾斯每日签到 任务开关
 
 [Script]
 #会员宠粉遇好礼
-cron {arg1} script-path=https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/refs/heads/main/Durex/Durex_Task01.js, enable= {arg2}, tag=会员宠粉遇好礼
+cron {cron1} script-path=https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/refs/heads/main/Durex/Durex_Task01.js, enable= {durex1}, tag=会员宠粉遇好礼
 
 #幸运转转转10月送好礼
-cron {arg3} script-path=https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/refs/heads/main/Durex/Durex_Task02.js, enable= {arg4}, tag=幸运转转转10月送好礼
+cron {cron2} script-path=https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/refs/heads/main/Durex/Durex_Task02.js, enable= {durex2}, tag=幸运转转转10月送好礼
 
 #杜蕾斯每日签到
-cron {arg5} script-path=https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/dls.js, enable= {arg6}, tag=杜蕾斯每日签到
+cron {cron3} script-path=https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/dls.js, enable= {durex3}, tag=杜蕾斯每日签到
 
 #获取Cookie
-http-request ^https:\/\/vip\.ixiliu\.cn\/mp\/user\/info$ script-path=https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/dls.js,enable= {arg0}, timeout=60, tag=杜蕾斯Cookie
+http-request ^https:\/\/vip\.ixiliu\.cn\/mp\/user\/info$ script-path=https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/dls.js,enable= {cookie}, timeout=60, tag=杜蕾斯Cookie
 
 [MITM]
 hostname = vip.ixiliu.cn
