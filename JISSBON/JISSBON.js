71f478a50ae40fe93cd2e8a7b431561f631bd899
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2024-10-23 17:41:45 +0800

    Create JISSBON.js

---
+++ JISSBON/JISSBON.js
@@ -0,0 +1,21 @@
+#!name = 杰士邦签到
+#!desc = 国产男子汉签到
+#!data = 2024-10-23
+#!tag = 签到
+#!icon = https://raw.githubusercontent.com/ROOOTCHEN/QuantumultX/refs/heads/main/JISSBON/JISSBON-Logo.png
+#!loon_version = 3.2.3(762)
+
+[Argument]
+cookie = switch,true,tag=杰士邦Token
+
+cron1 = input,"0 0 * * *",tag=每日签到 定时任务
+JISSBON = switch,true,tag=每日签到 任务开关
+
+
+[Script]
+
+#获取Cookie
+http-request ^https:\/\/xh-vip-api\.a-touchin\.com\/mp\/user\/info script-path=https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/dls.js,enable= {cookie}, timeout=60, tag=杰士邦TOKNE
+
+
+cron {cron1} script-path=https://gist.githubusercontent.com/Sliverkiss/6d15658e313eee254a55d2b97933587c/raw/jsb.js, enable= {JISSBON}, tag=杰士邦签到
