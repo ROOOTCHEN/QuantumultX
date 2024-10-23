d15ff50777a7c05029f7e4383d1b9f4b879989e7
ROOOTCHEN <106802918+ROOOTCHEN@users.noreply.github.com>
2024-10-23 17:59:27 +0800

    Update JISSBON.js

--- JISSBON/JISSBON.js
+++ JISSBON/JISSBON.js
@@ -7,15 +7,14 @@
 
 [Argument]
 cookie = switch,true,tag=杰士邦Token
-
 cron1 = input,"0 0 * * *",tag=每日签到 定时任务
 JISSBON = switch,true,tag=每日签到 任务开关
-
 
 [Script]
 
 #获取Cookie
-http-request ^https:\/\/xh-vip-api\.a-touchin\.com\/mp\/user\/info script-path=https://gist.githubusercontent.com/Sliverkiss/6d15658e313eee254a55d2b97933587c/raw/jsb.js,enable= {cookie}, timeout=60, tag=杰士邦TOKNE
+http-response ^https:\/\/xh-vip-api\.a-touchin\.com\/mp\/user\/info script-path=https://gist.githubusercontent.com/Sliverkiss/6d15658e313eee254a55d2b97933587c/raw/jsb.js,enable= {cookie}, timeout=60, tag=杰士邦TOKNE
+
 #签到
 cron {cron1} script-path=https://gist.githubusercontent.com/Sliverkiss/6d15658e313eee254a55d2b97933587c/raw/jsb.js, enable= {JISSBON}, tag=杰士邦签到
 
